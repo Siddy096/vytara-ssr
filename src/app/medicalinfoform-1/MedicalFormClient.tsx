0e197269ca48216a5a01e9ae269aa4de105376e1
Siddy096 <varhadisiddhesh8@gmail.com>
2025-12-23 19:28:17 +0530

    Medical Info Form Redirect Error Fix - 11

--- src/app/medicalinfoform-1/MedicalFormClient.tsx
+++ src/app/medicalinfoform-1/MedicalFormClient.tsx
@@ -428,9 +428,7 @@ export default function MedicalInfoFormUI() {
           <button
             type="submit"
             disabled={loadingUser}
-            className="flex items-center gap-2 bg-[#FF8000] text-white px-6 py-2 rounded-lg
-                      hover:bg-[#309898] disabled:opacity-50 disabled:cursor-not-allowed"
-          >
+            className="flex items-center gap-2 bg-[#FF8000] text-white px-6 py-2 rounded-lg cursor-hover">
             Next
           </button>
         </div>
