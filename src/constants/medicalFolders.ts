80782dd9c4f22cccd78d90483fb5cd55cb6d0706
Siddy096 <129279171+Siddy096@users.noreply.github.com>
2025-12-27 16:43:11 +0530

    Delete src/constants/medicalFolders.ts

--- src/constants/medicalFolders.ts
+++
@@ -1,9 +0,0 @@
-export const MEDICAL_FOLDERS = {
-  reports: 'reports',
-  prescriptions: 'prescriptions',
-  bills: 'bills',
-  insurance: 'insurance',
-} as const;
-
-export type MedicalFolder = keyof typeof MEDICAL_FOLDERS;
-
